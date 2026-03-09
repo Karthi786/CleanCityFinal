d2f0872fbdb9553fd61e89de74e0803e5efd9aed
Karthi786 <karthikeyan2909y@gmail.com>
2026-03-09 21:40:08 +0530

    logout option

--- frontend/js/api.js
+++ frontend/js/api.js
@@ -4,7 +4,7 @@
  */
 
 const API_BASE = window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1'
-    ? 'http://localhost:3000/api'
+    ? 'http://localhost:5001/api'
     : 'https://cleancityfinal.onrender.com/api';
 
 /**
